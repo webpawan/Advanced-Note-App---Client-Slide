7cc214b909a8789da1bcd223be9cc0d8c46900e2
webpawan <pawansaini66477@gmail.com>
2023-05-13 21:02:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap";
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
